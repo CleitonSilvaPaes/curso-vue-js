6bd2b84e8962da87347eccdb85f4025503a8f326
@cleiton.paes <cleiton.paes@aprtech.com.br>
2023-12-22 16:41:53 -0300

    resolvendo desafio-01

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el : '#desafio',
+    data: {
+        nome : 'Cleiton Silva Carvalho Paes',
+        idade : '23',
+        imagem : 'https://static.vecteezy.com/ti/fotos-gratis/t1/22653879-fantasia-ilha-com-cachoeiras-3d-ilustracao-elementos-do-isto-imagem-mobiliado-de-nasa-generativo-ai-gratis-foto.jpg'
+    },
+    methods: {
+        num_random() {return Math.random()}
+    }
+})
